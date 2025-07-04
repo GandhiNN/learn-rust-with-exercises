f58ea4993e2952a4a449e2aa409b4d01ba1ca502
Ngakan Gandhi <ngandhi@pmintl.net>
2025-07-04 12:35:33 +0700

    feat: add more exercise

--- exercises/02_a_basic_calculator/while_06/src/lib.rs
+++ exercises/02_a_basic_calculator/while_06/src/lib.rs
@@ -1,5 +1,12 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Rewrite the factorial function using a `while` loop
+pub fn factorial(n: u32) -> u32 {
+    let mut res = 1;
+    let mut i = 1;
+    while i <= n {
+        res *= i;
+        i += 1;
+    }
+    res
 }
 
 #[cfg(test)]
@@ -7,8 +14,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
     }
 }
